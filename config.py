889462aedbe62af323dd0db15a9a2ef22f53924a
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2022-12-20 07:28:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = 7108671
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = "51aaf9384a31af3e7068118883614867"
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = "5487546429:AAHZYxHGu2BCOJB-w5ZmxRq1Ni8cMOtP0AA"
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = "M4u_video"     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = "mongodb+srv://enrich:enrich@cluster0.mhghl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
  
-FLOOD = int(os.environ.get("FLOOD", "10"))
+FLOOD = "10"
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = "https://telegra.ph/file/3678dcf40bc9bfed7b79e.jpg"
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1121140181').split()]
 
